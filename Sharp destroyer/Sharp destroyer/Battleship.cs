c819325a8808f73407c6aa8410c40a9d035cbf4b
Evgeny <avalancher2@rambler.ru>
2017-05-20 14:07:49 +0300

    dddd

--- Sharp destroyer/Sharp destroyer/Battleship.cs
+++ Sharp destroyer/Sharp destroyer/Battleship.cs
@@ -3,18 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Sharp_destroyer
 {
     class Battleship
     {
         public CellType[,] OurField = new CellType[10, 10];
+        public CellType[,] EnemyField = new CellType[10, 10];
         public string SetUpShips()
         {
 
-            return "";
+            return "1,2";
+        }
+        public Point PointToFire()
+        {
+            return new Point(1,1);
         }
 
 
+    }
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public override string ToString()
+        {
+            return X.ToString() + "," + Y.ToString();
+        }
     }
 }
