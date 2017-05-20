9db263ff7e9bcfd020f61e2b2a29982e269e6dcc
Evgeny <avalancher2@rambler.ru>
2017-05-20 13:41:27 +0300

    ddd

--- Sharp destroyer/Sharp destroyer/Battleship.cs
+++ Sharp destroyer/Sharp destroyer/Battleship.cs
@@ -6,13 +6,21 @@ using System.Threading.Tasks;
 
 namespace Sharp_destroyer
 {
-    class Battleship
+    public class Battleship
     {
+        public CellType[,] OurField = new CellType[10, 10];
         public bool SetUpShips()
         {
             return true;
         }
 
 
+    }
+    public enum CellType
+    {
+        Empty,
+        Ship,
+        Wreck,
+        Mine
     }
 }
